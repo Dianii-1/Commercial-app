2f6338ac6c79a4b4c15d2b44a89620d42b5263d2
Dianii-1 <dianii230416@gmail.com>
2026-05-24 13:42:23 -0500

    feat: Create navbar

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+import { Button } from "@heroui/react";
+import { IoHome } from "react-icons/io5";
+import { CiViewList } from "react-icons/ci";
+import { MdAddComment } from "react-icons/md";
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-between w-full border-b border-gray-200 shadow-lg bg-white/70 backdrop-blur-md p-6">
+      <Link
+        href="/"
+        className="flex items-center gap-2 text-2xl font-black text-[#008296]"
+      >
+        <IoHome /> <span className="hidden sm:block">Inicio</span>
+      </Link>
+
+      <div className="flex items-center gap-4">
+        <Link href="/listado">
+          <CiViewList size="30px" className="sm:hidden text-[#FAC830]" />
+          <Button variant="outline" className="hidden sm:block font-semibold">
+            Publicaciones
+          </Button>
+        </Link>
+
+        <Link href="/listado/nuevo">
+          <MdAddComment size="30px" className="sm:hidden text-[#008296]" />
+          <Button className="hidden sm:block bg-[#008296] text-white p-2 px-6 rounded-xl">
+            Nueva publicación
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
